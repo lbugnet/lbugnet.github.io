5de50c56023b63834498e48a4ac6e4dcce60a22a
LBUG <lbugnet@flatironinstitute.org>
2021-11-10 11:18:41 -0800

    Update publications.js

--- src/data/publications.js
+++ src/data/publications.js
@@ -1,47 +1,42 @@
 // TODO Add a couple lines about each publication
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/publications/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: 'Magnetic signatures on mixed-mode frequencies.',
+    subtitle: 'I. An axisymmetric fossil field inside the core of red giants.',
+    journal: 'A&A, 650, A53 + journal cover',
+    link: 'https://www.aanda.org/articles/aa/abs/2021/06/aa39159-20/aa39159-20.html',
+    image: '/images/publications/big_photocouv650-2-2.jpg',
+    date: '2021-06',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'Bugnet, L., V. Prat, S. Mathis, A. Astoul, K. Augustson, R.A. García, S. Mathur, L. Amard, C. Neiner.  ',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/publications/harvest.jpg',
-    date: '2015-09-20',
+    title: 'Sounding the internal magnetism of starsusing asymptotic magneto-asteroseismolog',
+    journal: 'A&A, 647, A122',
+    link: 'https://www.aanda.org/articles/aa/full_html/2021/03/aa39180-20/aa39180-20.html',
+    image: '/images/publications/Fig3_Strategy.jpg',
+    date: '2021-03',
     desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+      'S. Mathis∗, L. Bugnet∗, V. Prat, K. Augustson, S. Mathur, and R. A. Garcia.',
+    supl: 'S. Mathis and L. Bugnet equally contributed to this work.',
   },
   {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/publications/spacepotato.jpg',
-    date: '2015-06-28',
+    title: 'FliPerClass: In search of solar-like pulsators among TESS targets',
+    journal: 'A&A, 624, A79',
+    link: 'https://www.aanda.org/articles/aa/abs/2019/04/aa34780-18/aa34780-18.html',
+    image: '/images/publications/fliperclass.png',
+    date: '2019-04',
     desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
+      'L. Bugnet, R. A. García, S. Mathur, G. R. Davies, O. J. Hall, M. N. Lund, and B. M. Rendle',
   },
   {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/publications/catdetector.jpg',
-    date: '2015-05-15',
+    title: 'FliPer: A global measure of power density to estimate surface gravities of main-sequence solar-like stars and red giants',
+    journal: 'A&A 620, A38',
+    link: 'https://www.aanda.org/articles/aa/full_html/2018/12/aa33106-18/aa33106-18.html',
+    image: '/images/publications/plot_fliper_main_new_july.png',
+    date: '2018-11',
     desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the publication too expensive '
-      + 'to continue hosting.',
+      'L. Bugnet, R. A. García, G. R. Davies, S. Mathur, E. Corsaro, O. J. Hall, and B. M. Rendle',
   },
 ];
 
