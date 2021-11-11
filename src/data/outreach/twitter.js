9c6ddb64ceadd05f0beeb35ba1cfbf7f2c775322
LBUG <lbugnet@flatironinstitute.org>
2021-11-10 23:40:10 -0800

    su

--- src/data/outreach/twitter.js
+++ src/data/outreach/twitter.js
@@ -6,11 +6,23 @@ const twitter = [
     journal: 'Episode 3: Le rire des étoiles',
     twitter: 'https://twitter.com/AudioSerious/status/1257913133848502272?ref_src=twsrc%5Etfw',
     // link: 'https://www.aanda.org/articles/aa/abs/2021/06/aa39159-20/aa39159-20.html',
-    image: '/images/outreach/twitterserious.png',
+    // image: '/images/outreach/twitterserious.png',
     date: '2020',
     // desc:
     //   'Mathur, S., [...], Bugnet, L. et al. ',
   },
+
+  // {
+  //   title: 'Posdcast "Talents Scientifiques"',
+  //   // subtitle: 'I. An axisymmetric fossil field inside the core of red giants.',
+  //   journal: 'Episode 3: Le rire des étoiles',
+  //   twitter: 'https://twitter.com/AudioSerious/status/1257913133848502272?ref_src=twsrc%5Etfw',
+  //   // link: 'https://www.aanda.org/articles/aa/abs/2021/06/aa39159-20/aa39159-20.html',
+  //   image: '/images/outreach/twitterserious.png',
+  //   date: '2020',
+  //   // desc:
+  //   //   'Mathur, S., [...], Bugnet, L. et al. ',
+  // },
 ];
 
 export default twitter;
